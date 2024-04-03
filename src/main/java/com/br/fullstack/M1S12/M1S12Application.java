52175880282fa7a7e1588fa4e545ad3e8e00898b
gustavobdambros <gustavo_dambros1@hotmail.com>
2024-04-03 19:40:12 -0300

    first commit

---
+++ src/main/java/com/br/fullstack/M1S12/M1S12Application.java
@@ -0,0 +1,13 @@
+package com.br.fullstack.M1S12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M1S12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M1S12Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/fullstack/M1S12/M1S12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.fullstack.M1S12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M1S12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
