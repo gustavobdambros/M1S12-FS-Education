25fba081c617b0c111244d662cc0e62cdbbf6608
gustavobdambros <gustavo_dambros1@hotmail.com>
2024-04-07 11:29:36 -0300

    [M1S12] Ex 6 - Lançamento de nota - feature DELETE

--- src/main/java/com/br/fullstack/M1S12/controller/NotasController.java
+++ src/main/java/com/br/fullstack/M1S12/controller/NotasController.java
@@ -2,18 +2,20 @@ package com.br.fullstack.M1S12.controller;
 
 import com.br.fullstack.M1S12.entity.NotasEntity;
 import com.br.fullstack.M1S12.service.NotasService;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
+@Slf4j
 @RequestMapping("/notas")
 public class NotasController {
 
@@ -32,5 +34,13 @@ public class NotasController {
         }
         logger.info("Notas encontradas para a matrícula ID: {}", matriculaId);
         return ResponseEntity.ok(notas);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deletarNota(@PathVariable Long id){
+        log.info("Chamada para DELETE no endpoint /nota/{id} recebida. ID: " + id);
+        ResponseEntity<?> responseEntity = notasService.deletarNota(id);
+        log.info("Finalizada chamada DELETE no endpoint /nota/{id}. Nota com o id " + id + "deletada com sucesso.");
+        return responseEntity;
     }
 }
