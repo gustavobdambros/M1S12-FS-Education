69f0a5ef315bd2e43a09f38749cd09b7b32af247
gustavobdambros <gustavo_dambros1@hotmail.com>
2024-04-07 10:56:12 -0300

    [M1S12] Ex 6 - Lançamento de nota - feature DELETE

--- src/main/java/com/br/fullstack/M1S12/controller/NotasController.java
+++ src/main/java/com/br/fullstack/M1S12/controller/NotasController.java
@@ -1,36 +1,31 @@
 package com.br.fullstack.M1S12.controller;
 
-import com.br.fullstack.M1S12.entity.NotasEntity;
 import com.br.fullstack.M1S12.service.NotasService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/notas")
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("nota")
 public class NotasController {
 
-    @Autowired
-    private NotasService notasService;
+    private final NotasService notasService;
 
-    private static final Logger logger = LoggerFactory.getLogger(NotasController.class);
+    private static Logger logger = LoggerFactory.getLogger(AlunoController.class);
 
-    @GetMapping("/matricula/{matriculaId}")
-    public ResponseEntity<List<NotasEntity>> buscarNotasPorMatricula(@PathVariable Long matriculaId) {
-        logger.info("Buscando notas para a matrícula ID: {}", matriculaId);
-        List<NotasEntity> notas = notasService.buscarNotasPorMatriculaId(matriculaId);
-        if (notas.isEmpty()) {
-            logger.info("Nenhuma nota encontrada para a matrícula ID: {}", matriculaId);
-            return ResponseEntity.notFound().build();
-        }
-        logger.info("Notas encontradas para a matrícula ID: {}", matriculaId);
-        return ResponseEntity.ok(notas);
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deletarNota(@PathVariable Long id){
+        log.info("Chamada para DELETE no endpoint /nota/{id} recebida. ID: " + id);
+        ResponseEntity<?> responseEntity = notasService.deletarNota(id);
+        log.info("Finalizada chamada DELETE no endpoint /nota/{id}. Nota com o id " + id + "deletada com sucesso.");
+        return responseEntity;
     }
 }
--- src/main/java/com/br/fullstack/M1S12/service/NotasService.java
+++ src/main/java/com/br/fullstack/M1S12/service/NotasService.java
@@ -1,20 +1,56 @@
 package com.br.fullstack.M1S12.service;
 
+import com.br.fullstack.M1S12.entity.DisciplinaMatriculaEntity;
 import com.br.fullstack.M1S12.entity.NotasEntity;
+import com.br.fullstack.M1S12.exception.NotFoundException;
+import com.br.fullstack.M1S12.repository.DisciplinaMatriculaRepository;
 import com.br.fullstack.M1S12.repository.NotasRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class NotasService {
 
-    @Autowired
-    private NotasRepository notasRepository;
+    private final NotasRepository notasRepository;
+    private final DisciplinaMatriculaRepository disciplinaMatriculaRepository;
 
-    public List<NotasEntity> buscarNotasPorMatriculaId(Long matriculaId) {
-        return notasRepository.buscarNotasPorMatriculaId(matriculaId);
+    private static final Logger logger = LoggerFactory.getLogger(DisciplinaMatriculaService.class);
+
+    public ResponseEntity<?> deletarNota(Long id) {
+        log.info("Processo de deleção de nota iniciado. ID recebido {}", id);
+
+        log.info("Tentativa de busca por nota em matrículas com o ID " + id + " para a exclusão...");
+        DisciplinaMatriculaEntity matricula = notasRepository.findById(id).map(NotasEntity::getDisciplina_matricula_id).orElseThrow(
+                () -> new NotFoundException("Nota não encontrada com o ID: " + id));
+        log.info("Nota encontrada e com matrícula associada. ID: " + id);
+
+        log.info("Atualizando os dados para recalcular a média final...");
+        double coeficiente = notasRepository.findById(id).get().getCoeficiente();
+        double valorNota = notasRepository.findById(id).get().getNota();
+        double novaMediaFinal = recalcularMediaFinal(matricula, coeficiente, valorNota);
+
+        log.info("Salvando matrícula atualizada no banco de dados...");
+        matricula.setMediaFinal(novaMediaFinal);
+        disciplinaMatriculaRepository.save(matricula);
+        log.info("Matrícula atualizada salva!");
+
+        notasRepository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    private double recalcularMediaFinal(DisciplinaMatriculaEntity matricula, double coeficienteExcluido, double valorNotaExcluida) {
+        log.info("Recalculando média final...");
+        double mediaFinalAtual = matricula.getMediaFinal();
+        double somaNotasAtuais = mediaFinalAtual * (1 - coeficienteExcluido);
+        double novaSomaNotas = somaNotasAtuais - (coeficienteExcluido * valorNotaExcluida);
+        log.info("Cálculo da média atualizado com sucesso.");
+        return novaSomaNotas / (1 - coeficienteExcluido);
     }
 }
-
